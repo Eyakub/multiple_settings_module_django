075274ea912538d1ba1a2205c7138a85b285d719
eyakubsorkar <eyakubsorkar@gmail.com>
2019-11-04 13:23:48 +0600

    done multiple settings module

---
+++ src/app/settings/development.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+# INSTALLED_APPS += [
+#     'debug_toolbar',
+# ]
+
+# MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# DEBUG_TOOLBAR_CONFIG = {
+#     'name_url': }
